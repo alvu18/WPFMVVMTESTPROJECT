7e215b08962b46d4df780634dd3d1309d04c7f88
alvu18 <118918898+alvu18@users.noreply.github.com>
2023-09-03 18:31:38 +0300

    Table Human
    Таблица Human

---
+++ Tables/dbo.Human.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Human] (
+  [IdHuman] [int] IDENTITY,
+  [Name] [varchar](50) NOT NULL,
+  [Surname] [varchar](50) NOT NULL,
+  [Fathername] [varchar](50) NULL,
+  [Number] [varchar](50) NOT NULL,
+  [Adress] [varchar](50) NOT NULL,
+  [Email] [varchar](50) NOT NULL,
+  CONSTRAINT [PK_Human_IdHuman] PRIMARY KEY CLUSTERED ([IdHuman])
+)
+ON [PRIMARY]
+GO
